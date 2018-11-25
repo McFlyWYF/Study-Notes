5cdf87d4d3198d0bcf8dbf105705f10df5c04101
wangyufei <848671907@qq.com>
2018-11-25 17:42:22 +0800

    :fire:JavaEE

---
+++ JavaEE/myapp/app/controllers/Course.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Course extends Controller {
+
+    public static void course(){
+
+        render();
+    }
+
+    public static void studycourse(){
+        render();
+    }
+
+    public static void deletesucc(){
+        render();
+    }
+}
